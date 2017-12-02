7a40231caff522568c50adf004fb68b3da67ea4a
June Kim <paragonjh@gmail.com>
2017-12-02 22:44:39 +0900

    Add Coinone Ticker

---
+++ ccapis/apis/REST/coinone.py
@@ -0,0 +1,50 @@
+"""
+Contains all API Client sub-classes, which store exchange specific details
+and feature the respective exchanges authentication method (sign()).
+"""
+# Import Built-ins
+import sys
+import time
+import math
+import base64
+import logging
+import hashlib
+import hmac
+
+# Import Homebrew
+from ccapis.apis.rest import RESTAPIClient
+
+
+log = logging.getLogger(__name__)
+
+
+class CoinoneREST(RESTAPIClient):
+    def __init__(self, user_id='', key=None, secret=None, api_version=None,
+                 url='https://api.coinone.co.kr', timeout=5):
+        self.id = user_id
+        super(CoinoneREST, self).__init__(url, api_version=api_version,
+                                           key=key, secret=secret,
+                                           timeout=timeout)
+
+    def sign(self, url, endpoint, endpoint_path, method_verb, *args, **kwargs):
+        nonce = self.nonce()
+        message = nonce + self.id + self.key
+
+        signature = hmac.new(self.secret.encode(), message.encode(),
+                             hashlib.sha256)
+        signature = signature.hexdigest().upper()
+
+        try:
+            req = kwargs['params']
+        except KeyError:
+            req = {}
+        req['key'] = self.key
+        req['nonce'] = nonce
+        req['signature'] = signature
+        return url, {'data': req}
+
+    def public_query(self, endpoint, **kwargs):
+        return self.query('GET', endpoint, **kwargs)
+
+    def private_query(self, endpoint, **kwargs):
+        return self.query('GET', endpoint, authenticate=True, **kwargs)
